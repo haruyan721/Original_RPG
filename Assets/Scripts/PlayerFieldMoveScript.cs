ea0f8ec02597c1767ecf641e6fe2e1f1627de0fb
合田　晴哉 <hgoda@oregano.ocn.ne.jp>
2017-07-01 21:45:42 +0900

    フィールドのシーンからバトルシーンへの切り替えを作成開始。スクリプト作動のON,OFFをシーン変化により切り替えられるようなコードを作成した。また、フィールドでのプレイヤーの移動も作成した。

---
+++ Assets/Scripts/PlayerFieldMoveScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFieldMoveScript : MonoBehaviour {
+
+	int moveSpeed = 15;
+	PlayerModeScript playerModeScript;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3(0,0,moveSpeed) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= new Vector3(0,0,moveSpeed) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= new Vector3(moveSpeed,0,0) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3(moveSpeed,0,0) * Time.deltaTime;
+		}
+
+	}
+}
