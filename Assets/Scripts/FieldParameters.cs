20d70d351f9a9df51eb9a94e8ddf1c77c832a708
合田　晴哉 <hgoda@oregano.ocn.ne.jp>
2017-07-05 19:52:19 +0900

    さっきのコミットがうまくいってなかったのでこちらが本物。

---
+++ Assets/Scripts/FieldParameters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldParameters : MonoBehaviour {
+
+	public Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
