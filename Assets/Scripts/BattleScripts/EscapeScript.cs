2d7233904a50292ba529d13a3d62dcfb71d6f693
合田　晴哉 <hgoda@oregano.ocn.ne.jp>
2017-07-30 10:13:19 +0900

    こっちも同じく

---
+++ Assets/Scripts/BattleScripts/EscapeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeScript : MonoBehaviour {
+	GameObject turnManagement;
+	TurnManager turnManager;
+	// Use this for initialization
+	void Start () {
+		turnManagement = GameObject.Find ("TurnManagement");
+		turnManager = turnManagement.GetComponent<TurnManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Escape(){
+
+	}
+}
